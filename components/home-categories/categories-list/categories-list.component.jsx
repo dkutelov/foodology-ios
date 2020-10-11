9ef86e11b901a0d832e0c748c35a96eedf4a168a
Dari <dari.k@abv.bg>
2020-10-11 11:13:59 +0300

    home categories update

--- components/home-categories/categories-list/categories-list.component.jsx
+++ components/home-categories/categories-list/categories-list.component.jsx
@@ -1,31 +1,38 @@
 import CategoryItem from '../category-item/category-item.component';
+import styles from './categories-list.module.css';
 
 const CategoriesList = ({ categories }) => {
     const mainCategories = new Set(categories.map((category) => category.main));
 
-    return Array.from(mainCategories).map((mainCategory) => {
-        const subCategories = categories.filter(
-            (category) => category.main === mainCategory
-        );
-        return (
-            <div
-                className='text-primaryText antialiased pt-6'
-                key={mainCategory}
-            >
-                <h3 className='text-center uppercase text-2xl text-semibold pt-6 pb-3'>
-                    {mainCategory}
-                </h3>
-                <div className='flex flex-wrap my-4'>
-                    {subCategories.map((subcategory) => (
-                        <CategoryItem
-                            subcategory={subcategory}
-                            key={subcategory.id}
-                        />
-                    ))}
-                </div>
-            </div>
-        );
-    });
+    return (
+        <div className={styles.category_list}>
+            {Array.from(mainCategories).map((mainCategory) => {
+                const subCategories = categories.filter(
+                    (category) => category.main === mainCategory
+                );
+                return (
+                    <div
+                        className={styles.category_list__item}
+                        key={mainCategory}
+                    >
+                        <div>
+                            <h3 className='text-center uppercase text-2xl text-semibold pt-6 pb-3'>
+                                {mainCategory}
+                            </h3>
+                            <div className={styles.subcategory_list__wrap}>
+                                {subCategories.map((subcategory) => (
+                                    <CategoryItem
+                                        subcategory={subcategory}
+                                        key={subcategory.id}
+                                    />
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
 };
 
 export default CategoriesList;
--- components/home-categories/category-item/category-item.component.jsx
+++ components/home-categories/category-item/category-item.component.jsx
@@ -1,18 +1,13 @@
 import Link from 'next/link';
-
+import styles from './category-item.module.css';
 const CategoryItem = ({ subcategory }) => {
     return (
-        <div className='w-1/3 mb-2'>
-            <Link href={`/listing/${subcategory.id}`}>
-                <a>
-                    <div className='mx-1 border border-sectionBorderColor rounded overflow-hidden shadow-sm hover:border-primary hover:shadow-lg'>
-                        <div className='px-12 pt-6 pb-6'>
-                            <div className='font-bold text-center'>
-                                {subcategory.name}
-                            </div>
-                        </div>
-                    </div>
-                </a>
+        <div className={styles.subcategory_list__item}>
+            <Link
+                href={`/listing/${subcategory.id}`}
+                className={styles.subcategory_list__link}
+            >
+                <a>{subcategory.name}</a>
             </Link>
         </div>
     );
