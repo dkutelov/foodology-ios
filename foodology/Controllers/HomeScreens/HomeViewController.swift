2fcda3de0a2b14d12767c44d994aa034fb0db32f
dkutelov <dkutelov@digitals.bg>
2022-02-05 12:17:07 +0200

    Menu segmented control apprearance

--- foodology/Controllers/HomeScreens/HomeViewController.swift
+++ foodology/Controllers/HomeScreens/HomeViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var menuSegmentControl: UISegmentedControl!
+    
+    // MARK: - Properties
+    
     var meals = [Meal]()
+    
+    // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = "Today's Meals"
         meals = mockData
+        
+        setUI()
     }
+    
+    
+    // MARK: - Private methods
+    private func setUI() {
+        if let font = UIFont(name: "Poppins-Light", size: 16){
+            menuSegmentControl.setTitleTextAttributes(
+                [NSAttributedString.Key.font: font], for: .normal)
+            menuSegmentControl.setTitleTextAttributes(
+                [NSAttributedString.Key.foregroundColor: UIColor.white], for: .selected)
+        }
+ 
+         //Family: Poppins Font names: ["Poppins-Regular", "Poppins-Light", "Poppins-Bold", "Poppins-ExtraBold"]
+    }
+}
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return meals.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
     
 }
